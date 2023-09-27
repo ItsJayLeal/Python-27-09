2cf3fec20330682213a3377ec933a6f0332fc08b
João Pedro Leal <146259111+ItsJayLeal@users.noreply.github.com>
2023-09-27 16:38:03 -0300

    Add files via upload

---
+++ Converter C para FAH.py
@@ -0,0 +1,17 @@
+Cel = int(input("Temperatura atual em C"))
+Fah = (Cel * 9/5) + 32
+
+print ("A temperatura atual de Celsius é de :", Cel)
+print ("A temperatura atual convertida para Fah é de : ",Fah)
+
+
+
+
+
+
+
+
+
+
+
+
