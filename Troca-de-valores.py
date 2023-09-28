0944675586b80ed4fb2e53ac47798fc6f95e22c6
João Pedro Leal <146259111+ItsJayLeal@users.noreply.github.com>
2023-09-27 22:29:17 -0300

    Add files via upload

---
+++ Troca-de-valores.py
@@ -0,0 +1,7 @@
+X = int(input('Digite agora o valor de x'))
+Y = int (input('Faça o mesmo para o valor de Y'))
+
+X,Y = Y,X
+
+print ("Vamos exibir agora o novo valor de X",X)
+print ("Vamos exibir agora o novo valor de Y",Y)
