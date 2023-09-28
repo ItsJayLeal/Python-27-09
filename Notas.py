3b9d9134f134436953e2d8929c8d7b8c5f9c8f04
João Pedro Leal <146259111+ItsJayLeal@users.noreply.github.com>
2023-09-28 10:07:45 -0300

    Add files via upload

---
+++ Notas.py
@@ -0,0 +1,18 @@
+#valor_T = int(input("O valor inicial será de :")) // Não vai precisar já que o exer pede um valor fornecido,que no caso estão a seguir.
+
+notas_50 = 200/50
+notas_10 = 100/10
+notas_1  = 300/1
+
+print("Você vai precisar de ",notas_50,"notas de 50 para pagar sua conta.")
+print("Você vai precisar de ",notas_10,"notasde 10 para pagar sua conta.")
+print("Você vai precisar de ",notas_1,"notas de 1 para pagar sua conta.")
+
+
+
+
+
+
+
+
+
